5506abe90b2bf7d16b74eb77cc36b8348cf10562
akarolak <artur.karolak@primaris.eu>
2017-02-15 15:46:27 +0100

    Issue: poprawki

---
+++ src/main/java/spring/Application.java
@@ -0,0 +1,16 @@
+package spring; /**
+ * Created by Inni on 2017-02-01.
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Application.class, args);
+
+    }
+}
---
+++ src/main/java/spring/testService/Service.java
@@ -0,0 +1,22 @@
+package spring.testService;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * Created by Artur Karolak on 2017-02-01.
+ */
+@RestController
+public class Service {
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, name));
+    }
+}
